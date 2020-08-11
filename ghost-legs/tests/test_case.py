9c82e874ee376d2729d946721e2cee8a1e58c337
AntoineC <antoine@a-f.studio>
2020-08-11 15:31:26 +0200

    wip (tcr)

--- ghost-legs/tests/test_case.py
+++ ghost-legs/tests/test_case.py
@@ -1,5 +1,5 @@
 from src.solution import solution
-from tests.__fixtures__.readings import readings1, readings2
+from tests.__fixtures__.readings import readings1, readings2, readings3
 
 
 def test_A():
@@ -10,4 +10,4 @@ def test_B():
     assert solution(readings2) == "A3 B5 C1 D2 E4"
 
 def test_C():
-    assert solution(readings2) == "A3 B5 C1 D2 E4"
+    assert solution(readings3) == "P3 Q7 R8 S5 T6 U2 V4 W1"
