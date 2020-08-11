48854edca344c24283294e6e3449016636e2edc4
AntoineC <antoine@a-f.studio>
2020-08-11 14:50:20 +0200

    wip (tcr)

---
+++ ghost-legs/src/solution.py
@@ -0,0 +1,76 @@
+readings = '''7 7
+A  B  C
+|  |  |
+|--|  |
+|  |--|
+|  |--|
+|  |  |
+1  2  3'''
+
+
+def getLines(readings):
+    return readings.splitlines()[1:]
+
+
+def mustGoLeft(line, currentIndex):
+    if currentIndex == 0:
+        return False
+    if line[currentIndex-1:currentIndex+1] == "|-":
+        return True
+    return line[currentIndex - 1] == "-"
+
+
+def mustGoRight(line, currentIndex):
+    if currentIndex + 1 == " ":
+        return False
+    if currentIndex + 1 == len(line):
+        return False
+    if line[currentIndex:currentIndex + 2] == "-|":
+        return True
+    return line[currentIndex + 1] == "-"
+
+
+def goRight(line, currentIndex):
+    result = currentIndex
+    while mustGoRight(line, result):
+        result += 1
+    return result
+
+
+def goLeft(line, currentIndex):
+    result = currentIndex
+    while mustGoLeft(line, result):
+        result -= 1
+    return result
+
+
+def getIdentifier(firstLine):
+    result = []
+    for i in range(len(firstLine)):
+        if firstLine[i] != " ":
+            result.append({
+                "char": firstLine[i],
+                "index": i,
+            })
+    return result
+
+
+def solution(readings):
+    lines = getLines(readings)
+
+    result = []
+    for identifier in getIdentifier(lines[0]):
+        currentIndex = identifier["index"]
+        for i in range(len(lines)):
+            if mustGoLeft(lines[i], currentIndex):
+                currentIndex = goLeft(lines[i], currentIndex)
+                continue
+            if mustGoRight(lines[i], currentIndex):
+                currentIndex = goRight(lines[i], currentIndex)
+                continue
+        result.append("{}{}".format(identifier["char"], lines[i][currentIndex]))
+
+    return " ".join(result)
+
+
+solution(readings)
---
+++ ghost-legs/tests/test_case.py
@@ -0,0 +1,70 @@
+from src.solution import solution
+from src.solution import mustGoRight
+from src.solution import mustGoLeft
+from src.solution import goRight
+from src.solution import goLeft
+from src.solution import getIdentifier
+
+readings1 = '''7 7
+A  B  C
+|  |  |
+|--|  |
+|  |--|
+|  |--|
+|  |  |
+1  2  3'''
+
+
+def test_must_go_right():
+    line = "|--|  |"
+    assert mustGoRight(line, 0) is True
+    assert mustGoRight(line, 1) is True
+    assert mustGoRight(line, 2) is True
+    assert mustGoRight(line, 6) is False
+
+
+def test_must_go_left():
+    line = "|--|  |"
+    assert mustGoLeft(line, 3) is True
+    assert mustGoLeft(line, 2) is True
+    assert mustGoLeft(line, 1) is True
+    assert mustGoLeft(line, 0) is False
+    assert mustGoLeft(line, 6) is False
+
+
+def test_go_right_as_long_as_you_can():
+    line = "|--|  |"
+    currentIndex = 0
+    assert goRight(line, currentIndex) == 3
+    assert goRight(line, 3) == 3
+    assert goRight(line, 6) == 6
+
+
+def test_go_left_as_long_as_you_can():
+    line = "|--|  |"
+    currentIndex = 3
+    assert goLeft(line, currentIndex) == 0
+    assert goLeft(line, 0) == 0
+    assert goLeft(line, 6) == 6
+
+
+def test_reads_starting_identifier():
+    firstLine = "A  B  C"
+    assert getIdentifier(firstLine) == [
+        {
+            "char": "A",
+            "index": 0,
+        },
+        {
+            "char": "B",
+            "index": 3,
+        },
+        {
+            "char": "C",
+            "index": 6,
+        },
+    ]
+
+
+def test_A():
+    assert solution(readings1) == "A2 B1 C3"
