b0e31c7b65be828d0e03acf9de1e83634aa50b50
AntoineC <antoine@a-f.studio>
2020-08-13 10:28:22 +0200

    Working solution

--- enigma/src/solution.py
+++ enigma/src/solution.py
@@ -1,30 +1,49 @@
-ALPHA = "ABCDEFGHEJKLMNOPQRSTUVWXYZ"
+import string
 
-def caesarShift(aString, startingNumber):
+def caesar(aString, startingNumber, decypher=False):
 
-    def getIndex(letter, startingNumber):
-        return (ALPHA.find(letter) + startingNumber) % 26
+    def getIndex(i):
+        indexInAlphabet = string.ascii_uppercase.find(aString[i])
+        if decypher:
+            return (indexInAlphabet - startingNumber - i) % 26
+        return (indexInAlphabet + startingNumber + i) % 26
 
     result = ""
-    for letter in aString:
-        result = result + ALPHA[(getIndex(letter, startingNumber))]
-        startingNumber += 1
+    for i in range(len(aString)):
+        result += string.ascii_uppercase[getIndex(i)]
+
     return result
 
 
+def decode(readings):
+    result = readings[-1]
+
+    for rotor in reversed(readings[2:5]):
+        result = mapRotor(rotor, result)
+
+    return caesar(result, int(readings[1]), True)
+
+def encode(readings):
+    result = caesar(readings[-1], int(readings[1]))
+
+    for rotor in readings[2:5]:
+        for j in range(len(result)):
+            result = result[:j] + rotor[string.ascii_uppercase.find(result[j])] + result[j+1:]
+
+    return result
+
+def mapRotor(rotor, aString):
+    mappedString = ""
+
+    for letter in aString:
+        mappedString += string.ascii_uppercase[rotor.find(letter)]
+
+    return mappedString
+
 def solution(readings):
-    rotors = [
-        "BDFHJLCPRTXVZNYEIWGAKMUSQO",
-        "AJDKSIRUXBLHWTMCQGZNPYFVOE",
-        "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-    ]
-
-    aString = "AAA"
-    result = caesarShift(aString, 4)
-
-    for i in range(len(rotors)):
-        for letter in result[-len(aString):]:
-            found = ALPHA.find(letter)
-            result += rotors[i][found]
-
-    return result[-len(aString):]
+    if readings[0] == "DECODE":
+        return decode(readings)
+    elif readings[0] == "ENCODE":
+        return encode(readings)
+    else:
+        raise ValueError("Could not read readings")
--- enigma/tests/test-solution.py
+++ enigma/tests/test-solution.py
@@ -1,18 +1,116 @@
 import unittest
-from src.solution import solution, caesarShift
+from src.solution import solution, caesar, mapRotor
+
+
+encode3 = '''ENCODE
+4
+BDFHJLCPRTXVZNYEIWGAKMUSQO
+AJDKSIRUXBLHWTMCQGZNPYFVOE
+EKMFLGDQVZNTOWYHXUSPAIBRCJ
+AAA'''
+
+encode23 = '''ENCODE
+7
+BDFHJLCPRTXVZNYEIWGAKMUSQO
+AJDKSIRUXBLHWTMCQGZNPYFVOE
+EKMFLGDQVZNTOWYHXUSPAIBRCJ
+WEATHERREPORTWINDYTODAY'''
+
+encode21 = '''ENCODE
+9
+BDFHJLCPRTXVZNYEIWGAKMUSQO
+AJDKSIRUXBLHWTMCQGZNPYFVOE
+EKMFLGDQVZNTOWYHXUSPAIBRCJ
+EVERYONEISWELCOMEHERE'''
+
+encode42 = '''ENCODE
+9
+BDFHJLCPRTXVZNYEIWGAKMUSQO
+AJDKSIRUXBLHWTMCQGZNPYFVOE
+EKMFLGDQVZNTOWYHXUSPAIBRCJ
+EVERYONEISWELCOMEHEREEVERYONEISWELCOMEHERE
+'''
+
+decode21 = '''DECODE
+9
+BDFHJLCPRTXVZNYEIWGAKMUSQO
+AJDKSIRUXBLHWTMCQGZNPYFVOE
+EKMFLGDQVZNTOWYHXUSPAIBRCJ
+PQSACVVTOISXFXCIAMQEM
+'''
+
+decode49 = '''DECODE
+5
+BDFHJLCPRTXVZNYEIWGAKMUSQO
+AJDKSIRUXBLHWTMCQGZNPYFVOE
+EKMFLGDQVZNTOWYHXUSPAIBRCJ
+XPCXAUPHYQALKJMGKRWPGYHFTKRFFFNOUTZCABUAEHQLGXREZ
+'''
 
 
 class SolutionTest(unittest.TestCase):
 
-    def test_example_input(self):
-        readings = '''ENCODE
-            4
-            BDFHJLCPRTXVZNYEIWGAKMUSQO
-            AJDKSIRUXBLHWTMCQGZNPYFVOE
-            EKMFLGDQVZNTOWYHXUSPAIBRCJ
-            AAA'''
-        self.assertEqual(solution("AAA"), "KQF")
-
     def test_caesar_shift(self):
-        self.assertEqual(caesarShift("AAA", 4), "EFG")
-        self.assertEqual(caesarShift("ZZZ", 1), "ABC")
+        self.assertEqual(
+            caesar("AAA", 4),
+            "EFG"
+        )
+        self.assertEqual(
+            caesar("ZZZ", 1),
+            "ABC"
+        )
+        self.assertEqual(
+            caesar("ABCD", 4),
+            "EGIK"
+        )
+        self.assertEqual(
+            caesar("EGIK", 4, True),
+            "ABCD",
+        )
+        self.assertEqual(
+            caesar("EFG", 4, True),
+            "AAA"
+        )
+
+    def test_mapRotor(self):
+        rotor = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+        inText = "KFDI"
+        self.assertEqual(mapRotor(rotor, inText), "BDGV")
+
+        rotor = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+        inText = "BDGV"
+        self.assertEqual(mapRotor(rotor, inText), "JCRX")
+
+        rotor = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+        inText = "JCRX"
+        self.assertEqual(mapRotor(rotor, inText), "EGIK")
+
+
+    def test_encode(self):
+        self.assertEqual(
+            solution(encode3.splitlines()),
+            "KQF"
+        )
+        self.assertEqual(
+            solution(encode23.splitlines()),
+            "ALWAURKQEQQWLRAWZHUYKVN"
+        )
+        self.assertEqual(
+            solution(encode21.splitlines()),
+            "PQSACVVTOISXFXCIAMQEM"
+        )
+        self.assertEqual(
+            solution(encode42.splitlines()),
+            "PQSACVVTOISXFXCIAMQEMDZIXFJJSTQIENEFQXVZYV"
+        )
+
+    def test_decode(self):
+        self.assertEqual(
+            solution(decode21.splitlines()),
+            "EVERYONEISWELCOMEHERE"
+        )
+
+        self.assertEqual(
+            solution(decode49.splitlines()),
+            "THEQUICKBROWNFOXJUMPSOVERALAZYSPHINXOFBLACKQUARTZ"
+        )
