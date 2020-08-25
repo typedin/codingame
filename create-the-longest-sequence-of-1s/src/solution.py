878ad927c8bf607f23d93575da1260e18247d2bf
AntoineC <antoine@a-f.studio>
2020-08-25 12:10:28 +0200

    working solution

--- create-the-longest-sequence-of-1s/src/solution.py
+++ create-the-longest-sequence-of-1s/src/solution.py
@@ -1,32 +1,9 @@
-import pdb
-
-
-def solution(readings="01010"):
-    chars = [char for char in readings]
-
-    max_sequ = 1
+def solution(readings):
+    arrays_of_ones = readings.split('0')
     count = 1
-    flip_used = False
-    flip_used_index = -1
 
-    for i in range(len(chars)):
-        if chars[i] == "1":
-            pdb.set_trace()
-            count += 1
-            max_sequ = max(count, max_sequ)
-            continue
-        if i + 1 < len(chars):
-            pdb.set_trace()
-            if (chars[i + 1] == "1"):
-                if count > 0:
-                    if not flip_used:
-                        flip_used = True
-                        flip_used_index = i
-        else:
-            pdb.set_trace()
-            if flip_used_index >= 0 and flip_used:
-                i = flip_used_index
-            count = 1
-            flip_used = False
-
-    return max_sequ
+    for i in range(len(arrays_of_ones) - 1):
+        count = max(count,
+                    len(arrays_of_ones[i] + arrays_of_ones[i+1]) + 1
+                    )
+    return count
